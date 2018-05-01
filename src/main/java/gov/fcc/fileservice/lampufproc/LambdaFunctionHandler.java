cd3841949b79811ce31d4f513330760adff41dcd
Alexandre Efremov <Alexandre.Efremov@fcc.gov>
2018-05-01 14:35:09 -0400

    AWS Lambda Aspose file processor code base

---
+++ src/main/java/gov/fcc/fileservice/lampufproc/LambdaFunctionHandler.java
@@ -0,0 +1,50 @@
+package gov.fcc.fileservice.lampufproc;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.google.gson.*;
+
+import com.amazonaws.services.lambda.runtime.events.SNSEvent;
+
+public class LambdaFunctionHandler implements RequestHandler<SNSEvent, String> {
+
+	String bucket, uuid, fileName, hook, reportBack;
+	JsonArray process;
+	
+    @Override
+    public String handleRequest(SNSEvent event, Context context) {
+        context.getLogger().log("Received event: " + event);
+        String message = event.getRecords().get(0).getSNS().getMessage();
+        context.getLogger().log("From SNS: " + message);
+        
+        ParseIncomingMessage(message);
+        
+        context.getLogger().log("Bucket : " + bucket);
+        context.getLogger().log("UUID : " + uuid);
+        context.getLogger().log("FileName : " + fileName);
+        context.getLogger().log("Process : " + process);
+        context.getLogger().log("Report Back : " + reportBack);
+        context.getLogger().log("Hook : " + hook);        	
+        
+        return message;
+    }
+    
+    private void ParseIncomingMessage(String message) {
+        JsonObject jsonMessage = new JsonParser().parse(message).getAsJsonObject();
+        
+        bucket = jsonMessage.get("bucket").getAsString();
+        uuid = jsonMessage.get("uuid").getAsString();
+        fileName = jsonMessage.get("fileName").getAsString();
+        reportBack = jsonMessage.get("reportBack").getAsString();
+        process = jsonMessage.get("process").getAsJsonArray();
+
+        JsonElement jhook = jsonMessage.get("hook");
+        if (jhook != null) {
+            hook = jhook.getAsString();
+        } else {
+            hook = null;
+        }
+    	
+    }
+    
+}
